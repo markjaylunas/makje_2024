295b67c7aeddcd34b2652645fb3840f7f249387e
Mark Jay Lunas <markjay.lunas@gmail.com>
2024-06-02 16:29:43 +0800

    fix spotlight responsive

--- src/components/hero/hero-section.tsx
+++ src/components/hero/hero-section.tsx
@@ -5,18 +5,18 @@ import { Spotlight } from "./spotlight";
 
 export default function HeroSection() {
   return (
-    <section className="h-[calc(100dvh-4rem)] px-8 md:px-24 flex flex-col items-center justify-evenly">
+    <section className="relative h-[calc(100dvh-4rem)] max-w-6xl px-8 md:px-24 flex flex-col items-center justify-evenly">
       <Spotlight
-        className="-top-0 left-0 md:-top-28 md:left-24 lg:-top-56 lg:left-72"
+        className="-top-16 left-0 md:left-44 md:-top-20"
         fill="white"
       />
 
       <Icons.logo
-        className=" size-44 relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] "
+        className="size-32 md:size-44 relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] "
         aria-label="Makje"
       />
 
-      <div>
+      <div className="flex flex-col items-center">
         <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
           I&apos;m Makje. <br /> A web developer.
         </h1>
@@ -25,15 +25,9 @@ export default function HeroSection() {
           Crafting a faster, personalized web with modern frontend technologies.
         </p>
 
-        <div className="mt-12 md:mt-8 flex gap-4 md:gap-8 justify-center items-center flex-wrap">
-          <Button className="p-0 w-fit h-fit ">
-            <ShinnyWrapper>Get in Touch</ShinnyWrapper>
-          </Button>
-
-          <Button className="p-0 w-fit h-fit">
-            <ShinnyWrapper>Get in Touch</ShinnyWrapper>
-          </Button>
-        </div>
+        <Button className="mt-12 md:mt-8 p-0 w-fit h-fit">
+          <ShinnyWrapper>Get in Touch</ShinnyWrapper>
+        </Button>
       </div>
     </section>
   );

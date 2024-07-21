a0b847c211deddfcf0cd505da6fdf9764a6d1cca
Mark Jay Lunas <markjay.lunas@gmail.com>
2024-07-21 17:21:46 +0800

    change padding on mobile

--- src/components/tech-stack/tech-stack-section.tsx
+++ src/components/tech-stack/tech-stack-section.tsx
@@ -2,7 +2,7 @@ import TechStackList from "./tech-stack-list";
 
 export default function TechStackSection() {
   return (
-    <section className="space-y-12 pt-16">
+    <section className="space-y-12 pt-10 sm:pt-16">
       <h2 className="text-3xl font-bold text-center">My Toolkit</h2>
       <TechStackList />
     </section>

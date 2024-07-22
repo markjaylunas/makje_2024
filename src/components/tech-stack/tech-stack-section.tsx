6134f9e09c0e2299b2b00ca34559e97c07c7d7ae
Mark Jay Lunas <markjay.lunas@gmail.com>
2024-07-22 13:10:02 +0800

    refactor title layout

--- src/components/tech-stack/tech-stack-section.tsx
+++ src/components/tech-stack/tech-stack-section.tsx
@@ -3,7 +3,9 @@ import TechStackList from "./tech-stack-list";
 export default function TechStackSection() {
   return (
     <section className="space-y-12 pt-10 sm:pt-16">
-      <h2 className="text-3xl font-bold text-center">My Toolkit</h2>
+      <h2 className="text-4xl font-bold text-center bg-gradient-to-b from-gray-50 to-gray-400 bg-clip-text text-transparent">
+        The Toolkit
+      </h2>
       <TechStackList />
     </section>
   );

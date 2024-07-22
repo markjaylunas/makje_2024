5bf457640b35ab90f3c587a79da3d3e628c25222
Mark Jay Lunas <markjay.lunas@gmail.com>
2024-07-22 14:18:40 +0800

    Add button icon

--- src/components/tech-stack/tech-stack-list.tsx
+++ src/components/tech-stack/tech-stack-list.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import { techStackList } from "@/lib/tech-stack-constants";
+import { cn } from "@/lib/utils";
 import { Button } from "@nextui-org/react";
 import { useState } from "react";
+import { Icons } from "../ui/icons";
 import TechCard from "./tech-card";
 
 export default function TechStackList() {
@@ -16,7 +18,13 @@ export default function TechStackList() {
         <TechCard key={tech.title} {...tech} />
       ))}
       <figure className="relative border border-neutral-800 border-dashed p-1 w-40 sm:w-48 lg:w-56 aspect-4/3 cursor-pointer overflow-hidden flex justify-center items-center">
-        <Button variant="bordered" onClick={() => setIsShowAll((v) => !v)}>
+        <Button
+          variant="bordered"
+          onClick={() => setIsShowAll((v) => !v)}
+          endContent={
+            <Icons.chevronDown className={cn(isShowAll && "rotate-180")} />
+          }
+        >
           {!isShowAll ? "Show all tech" : "Collapse"}
         </Button>
       </figure>
